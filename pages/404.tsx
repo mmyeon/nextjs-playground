fda8d7b43eeb7b60e04622d6c856855e31f2fb65
mmyeon <mindfulyeon@gmail.com>
2024-05-24 10:18:38 +0900

    [chore] Update prettierrc

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,10 +1,5 @@
-
 const My404Page = () => {
-    return (
-        <h1>
-           페이지를 찾을 수 없습니다. 
-        </h1>
-    );
+  return <h1>페이지를 찾을 수 없습니다.</h1>;
 };
 
 export default My404Page;
--- pages/500.tsx
+++ pages/500.tsx
@@ -1,10 +1,5 @@
-
 const My500Page = () => {
-    return (
-        <h1>
-            서버에서 에러가 발생했습니다.
-        </h1>
-    );
+  return <h1>서버에서 에러가 발생했습니다.</h1>;
 };
 
 export default My500Page;
